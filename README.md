d91d13069f614693ddf58cc8b3a29cdfd1d53e7c
David Fong <dfong633@gmail.com>
2021-12-19 15:06:27 -0500

    Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Birthday Project --> I wanted to create some code where I could store people's birthday and recieve a notification on that day.
+
+This is part two of my project.
+
+This code sends a request to my birthday API to recieve all the birthday's. It then parses the data and sends me a text message if there are any birthday's.
+
+This notification is sent using the Twilio API.
---
+++ main.py
@@ -0,0 +1,39 @@
+from twilio.rest import Client
+from credentials import account_sid, auth_token, my_cell, my_twilio
+from datetime import date
+import requests
+
+
+def send_message(client, my_msg):
+
+    client.messages.create(to=my_cell, from_=my_twilio, body=my_msg)
+
+base = "http://127.0.0.1:5000/"
+
+res = requests.get(base + "people").json()
+
+
+today = str(date.today())
+curDay = today.split("-")[2]
+curMonth = today.split("-")[1]
+
+
+message = []
+
+for birthdays in res.values():
+
+    day = birthdays["birthday"].split("/")[1]
+    month = birthdays["birthday"].split("/")[0]
+
+    if day == curDay and month == curMonth:
+
+        message.append(birthdays)
+    
+
+client = Client(account_sid, auth_token)
+
+
+for people in message:
+
+    my_msg = "Wish {0} a happy birthday!".format(people["name"])
+    send_message(client, my_msg)
